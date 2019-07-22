1777fdccbe283388e2567225c61af6e2947a2910
LMVR90 <luzmarcelavasquez@gmail.com>
2019-07-22 00:22:45 -0500

    Agrega mapa simple, ajusta algunos estilos

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/*let initMap = () => {
+    var laboratoria = {lat: 19.421489, lng: -99.163237};
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: laboratoria});
+    var marker = new google.maps.Marker({position: laboratoria, map: map});
+    return marker;
+};*/
